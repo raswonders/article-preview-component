e1c82953bcd1a66a29cd4be16b279de6b160d833
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-26 17:57:46 +0200

    add data & html for article

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import article from "./data.json";
+
 function App() {
   return (
     <>
-      <h1>rtt-vite-template</h1>
+      <article className="">
+        <header className="">
+          <img src={article.image.url} alt={article.image.alt} className="" />
+        </header>
+        <section className="">
+          <h1 className="">{article.title}</h1>
+          <p className="">{article.content}</p>
+        </section>
+        <footer className="">
+          <img
+            src={article.author.image.url}
+            alt={article.author.image.alt}
+            className=""
+          />
+          <div className="">
+            <h2 className="">{article.author.name}</h2>
+            <h3 className="">{article.author.date}</h3>
+          </div>
+          <button className="">Share</button>
+        </footer>
+      </article>
     </>
   );
 }
