15cd70cb9af956f789c10121c3dc53766d987872
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-30 11:48:37 +0200

    fix: text share isn't same size everywhere

--- src/components/SharePopover.tsx
+++ src/components/SharePopover.tsx
@@ -7,7 +7,7 @@ export function SharePopover({ open }: Props) {
     open && (
       <div className="absolute right-1/2 translate-x-1/2 -top-4 -translate-y-full px-8 py-4 hidden lg:flex lg:gap-4 lg:items-center bg-dark-gray rounded-[10px] shadow-[0px_10px_10px_rgba(201,213,225,0.50)]">
         <div className="absolute -bottom-[10px] right-1/2 translate-x-1/2 border-t-dark-gray border-t-[12px] border-x-[12px] border-x-transparent"></div>
-        <span className="text-xs tracking-[5px] text-[#9DAEC2] uppercase">
+        <span className="text-sm tracking-[5px] text-[#9DAEC2] uppercase">
           Share
         </span>
         <img
